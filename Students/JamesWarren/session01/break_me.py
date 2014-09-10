c20665bc60c3b820d629c432f140e90cdad2d14c
James Warren <jwarren116@gmail.com>
2014-09-10 16:03:43 -0700

    Initial framework for excercise created

---
+++ Students/JamesWarren/session01/break_me.py
@@ -0,0 +1,26 @@
+#NameError################
+def names(name):
+    pass
+
+names()
+
+
+#TypeError################
+def types(a, b):
+    pass
+
+types()
+
+
+#SyntaxError###############
+def syntax(n):
+    pass
+
+syntax()
+
+
+#AttributeError#############
+def attributes(k):
+    pass
+
+attributes()
