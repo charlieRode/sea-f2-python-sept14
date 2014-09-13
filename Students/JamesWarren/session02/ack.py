c06adf9839038a4b67495e0df90867b6981d3b01
James Warren <jwarren116@gmail.com>
2014-09-12 21:21:53 -0700

    Remove errors from calculations, correct syntax

--- Students/JamesWarren/session02/ack.py
+++ Students/JamesWarren/session02/ack.py
@@ -1,5 +1,8 @@
 def ack(m, n):
-    if m == 0: n+1
-    if m > 0  and  n == 0: ack(m-1, 1)
-    if m > 0  and  n > 0: ack(m-1, ack(m, n-1))
-    print locals()
+    if m == 0:
+        return n+1
+        return n
+    if m > 0  and  n == 0:
+        return ack(m-1, 1)
+    if m > 0  and  n > 0:
+        return ack(m-1, ack(m, n-1))
