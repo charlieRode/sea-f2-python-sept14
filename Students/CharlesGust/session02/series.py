51c96d226f5efae7d0becece68db4647a9d3b33a
Charles Gust <charlesg@equality.net>
2014-09-14 03:56:24 -0700

    add lucas function, general series function, 1st number is not 0th

--- Students/CharlesGust/session02/series.py
+++ Students/CharlesGust/session02/series.py
@@ -1,6 +1,17 @@
+def series(n, seed1=0, seed2=1):
+    if n <= 2:
+        if n == 1:
+            return seed1
+        elif n == 2:
+            return seed2
+        else:
+            return None
+    return series(n-1, seed1, seed2)+series(n-2, seed1, seed2)
+
+
+def lucas(n):
+    return series(n, 2, 1)
+
+
 def fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibonacci(n-1)+fibonacci(n-2)
+    return series(n)
