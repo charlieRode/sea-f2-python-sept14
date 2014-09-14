69f3c4af7550d0013648e382037398e2fde28d2b
James Warren <jwarren116@gmail.com>
2014-09-13 21:10:07 -0700

    Add Lucas's Number function

--- Students/JamesWarren/session02/series.py
+++ Students/JamesWarren/session02/series.py
@@ -5,6 +5,15 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
+def lucas(nth):
+    """Compute Lucas's number from input value."""
+    if nth == 1:
+        return nth + 1
+    if nth == 2:
+        return nth - 1
+    else:
+        return lucas(nth - 1) + lucas(nth - 2)
+
 if __name__ == "__main__":
     assert fibonacci(1) == 0
     assert fibonacci(5) == 3
