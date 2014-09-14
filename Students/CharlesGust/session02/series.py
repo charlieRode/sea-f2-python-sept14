3a0dabc87cd96f048ca1724270e90ee5916f9ab0
Charles Gust <charlesg@equality.net>
2014-09-14 03:59:25 -0700

    add docstring for functions

--- Students/CharlesGust/session02/series.py
+++ Students/CharlesGust/session02/series.py
@@ -1,4 +1,5 @@
 def series(n, seed1=0, seed2=1):
+    """ return the nTH number of a series seeded with seed1 and seed2 """
     if n <= 2:
         if n == 1:
             return seed1
@@ -10,8 +11,10 @@ def series(n, seed1=0, seed2=1):
 
 
 def lucas(n):
+    """ return the nTH Lucas number """
     return series(n, 2, 1)
 
 
 def fibonacci(n):
+    """ return the nTH Fibonacci number """
     return series(n)
