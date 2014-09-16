5252b66af34c48fa0733ba5fd39cd99699ff5d2d
James Warren <jwarren116@gmail.com>
2014-09-16 12:08:30 -0700

    Fix typo

--- Students/JamesWarren/session02/ack.py
+++ Students/JamesWarren/session02/ack.py
@@ -2,7 +2,6 @@ def ack(m, n):
     """Compute Ackermann's Function from input values."""
     if m == 0:
         return n+1
-        return n
     if m > 0  and  n == 0:
         return ack(m-1, 1)
     if m > 0  and  n > 0:
