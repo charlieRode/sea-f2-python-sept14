482d2c925b9ad271ffc4c4d39f742a352287bed9
Casey MacPhee <caseymacphee@gmail.com>
2014-09-15 18:44:41 -0700

    The error in ack had to do with tab being changed off of being 4 spaces for some reason

--- Students/CaseyMacPhee/session02/ack.py
+++ Students/CaseyMacPhee/session02/ack.py
@@ -1,13 +1,14 @@
 def ack(m, n):
-	"""Computes the Ackerman algorithm for ack(m,n)"""
-	if m < 0 or n < 0:
-		return None
+    """Computes the Ackerman algorithm for ack(m,n)"""
+
+    if m < 0 or n < 0:
+        return None
     if m == 0:
-		return n + 1
+        return n + 1
     if m > 0 and n == 0:
-		return ack(m - 1, 1)
+        return ack(m - 1, 1)
     if m > 0 and n > 0:
-		return ack(m - 1, ack(m, n - 1))
+        return ack(m - 1, ack(m, n - 1))
 
 
 assert ack(1,2) == 4
