1722ad1558cb20d0b5b6f892f2d4a4b8d1857c07
Casey MacPhee <caseymacphee@gmail.com>
2014-09-15 18:37:12 -0700

    Argg still working through some errors. Not sure why I'm getting an 'IndentationError: expected an indented block' wasn't getting that before I put in the """statement"""

--- Students/CaseyMacPhee/session02/ack.py
+++ Students/CaseyMacPhee/session02/ack.py
@@ -1,12 +1,15 @@
-def ack(m,n):
+def ack(m, n):
 	"""Computes the Ackerman algorithm for ack(m,n)"""
-    if m < 0 or n < 0:
-    	return None
+	if m < 0 or n < 0:
+		return None
     if m == 0:
 		return n + 1
     if m > 0 and n == 0:
 		return ack(m - 1, 1)
     if m > 0 and n > 0:
-    	return ack(m - 1, ack(m, n - 1))
+		return ack(m - 1, ack(m, n - 1))
 
-print ack(3, 4)
+
+assert ack(1,2) == 4
+assert ack(2,3) == 9
+assert ack(3,4) == 125
