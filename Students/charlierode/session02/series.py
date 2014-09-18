6ffab0c8626e0f95484ed89c5e329171e247587b
Charles Rode <chrode10@gmail.com>
2014-09-17 17:44:31 -0700

    delete duplicate files and directories. Make spelling correction in break_me.py. Correct initial values in series.py from 1, 2 to 0, 1

--- Students/charlierode/session02/series.py
+++ Students/charlierode/session02/series.py
@@ -1,5 +1,5 @@
 def fibonacci(n):
-	"""Returns the Nth Fibonacci number, where f(n) = f(n - 1) + f(n - 2), f(1) = f(2) = 1"""
+	"""Returns the Nth Fibonacci number, where f(n) = f(n - 1) + f(n - 2), f(0) = f(1) = 1"""
 	if n < 0: 
 		return None
 	elif n == 0: 
@@ -10,7 +10,7 @@ def fibonacci(n):
 		return fibonacci(n - 1) + fibonacci(n - 2)
 
 def lucas(n):
-	"""Returns the Nth Lucas number, where l(n) = l(n - 1) + l(n - 2), l(1) = 2, l(2) = 1"""
+	"""Returns the Nth Lucas number, where l(n) = l(n - 1) + l(n - 2), l(0) = 2, l(1) = 1"""
 	if n < 0:
 		return None
 	elif n == 0:
@@ -21,7 +21,7 @@ def lucas(n):
 		return lucas(n - 1) + lucas(n - 2)
 
 def sum_series(n, n1 = 0, n2 = 1):
-	"""Returns the Nth number in the series, where s(n) = s(n - 1) + s(n - 2), s(1) = n1, s(2) = n2"""
+	"""Returns the Nth number in the series, where s(n) = s(n - 1) + s(n - 2), s(0) = n1, s(1) = n2"""
 	if n < 0:
 		return None
 	elif n == 0:
