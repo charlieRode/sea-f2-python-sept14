2985d20e83423823dbd8151d1cf56c0c4a8e9cf8
James Warren <jwarren116@gmail.com>
2014-09-19 21:48:40 -0700

    Complete part 2 of dictionary_lab.py

--- Students/JamesWarren/session04/dictionary_lab.py
+++ Students/JamesWarren/session04/dictionary_lab.py
@@ -16,3 +16,8 @@ print 'cake' in dict
 print 'Mango' in dict.values()
 
 #PART 2
+d = {}
+d1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+d2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+for k, v in zip(d1, d2):
+    d[k] = v
