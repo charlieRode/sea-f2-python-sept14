09385296770a6ee3dde6be6da7f63e1dac8a1bbc
jbbrokaw <jbrokaw@gmail.com>
2014-09-21 11:18:39 -0700

    Implements bonus of series 2

--- Students/JasonBrokaw/session03/list_lab.py
+++ Students/JasonBrokaw/session03/list_lab.py
@@ -14,7 +14,7 @@ print fruits
 
 index = int(raw_input(u"Type an index from 1 through %i: " % len(fruits)))
 
-print "The %ith item in the list is %s" % (index, fruits[index - 1])
+print "Item %i in the list is %s" % (index, fruits[index - 1])
 
 fruits = [u"Cantaloupe"] + fruits
 
@@ -38,8 +38,24 @@ fruits2.pop()
 
 print fruits2
 
-fruit_to_delete = raw_input(u"Type a fruit to delete from the list: ")
-
-fruits2.remove(fruit_to_delete)
+fruits2 = 2 * fruits2
 
 print fruits2
+
+fruit_to_delete = raw_input(u"Type a fruit to delete from the list: ")
+
+while True:
+    try:
+        fruits2.index(fruit_to_delete)
+        break
+    except ValueError:
+        fruit_to_delete = raw_input(u"That fruit wasn't in the list. Try again: ")
+
+while True:
+    try:
+        fruits2.remove(fruit_to_delete)
+    except ValueError:
+        break
+
+print fruits2
+
