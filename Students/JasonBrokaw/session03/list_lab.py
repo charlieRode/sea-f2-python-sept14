79b853aef4473fa95bded1052aa1a17a6a20608b
jbbrokaw <jbrokaw@gmail.com>
2014-09-21 11:02:46 -0700

    Completes first series of list tasks

--- Students/JasonBrokaw/session03/list_lab.py
+++ Students/JasonBrokaw/session03/list_lab.py
@@ -4,3 +4,24 @@ fruits = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
 
 print fruits
 
+new_fruit = raw_input(u"Type a fruit: ")
+
+fruits.append(new_fruit)
+
+print fruits
+
+index = int(raw_input(u"Type an index from 1 through %i: " % len(fruits)))
+
+print "The %ith item in the list is %s" % (index, fruits[index - 1])
+
+fruits = [u"Cantaloupe"] + fruits
+
+print fruits
+
+fruits.insert(0, u"Banana")
+
+print fruits
+
+for fruit in fruits:
+    if fruit[0] == "P":
+        print fruit
