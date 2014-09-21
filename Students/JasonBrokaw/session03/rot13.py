74c4ede41317afedf0bcf312fb32eb5d6f68aa96
jbbrokaw <jbrokaw@gmail.com>
2014-09-21 12:08:36 -0700

    Adds tests to if __name__ == '__main__'

--- Students/JasonBrokaw/session03/rot13.py
+++ Students/JasonBrokaw/session03/rot13.py
@@ -9,3 +9,10 @@ def rot13(str):
     encryption = string.maketrans(alphabet + lowercase_alphabet, encrypted_alphabet + encrypted_lowercase_alphabet)
     return str.translate(encryption)
 
+if __name__ == '__main__':
+    test_string = "Hello, world. Here is a test string. 1, 2; 3"
+    assert test_string == rot13(rot13(test_string))
+    assert rot13("A") == "N"
+    assert rot13("a") == "n"
+    assert rot13(" ?^%81") == " ?^%81"
+    print "All Tests Pass"
