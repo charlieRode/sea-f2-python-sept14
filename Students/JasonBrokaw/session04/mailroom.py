425c9d2e42c3edb1f2cc134eedc4784f70efcc59
jbbrokaw <jbrokaw@gmail.com>
2014-09-21 23:09:49 -0700

    Changes data structures in mailroom to dicts

--- Students/JasonBrokaw/session04/mailroom.py
+++ Students/JasonBrokaw/session04/mailroom.py
@@ -1,13 +1,16 @@
 #!/usr/local/bin/python
 
-donor_history = [(u"Colonel Mustard", [100, 200, 300])]
-donor_history.append((u"Mrs. White", [50, 10.95]))
-donor_history.append((u"Professor Plum", [300, 300]))
-donor_history.append((u"Mr. Green", [1000, 2000]))
-donor_history.append((u"Miss Scarlet", [1000]))
+donor_history = {u"Colonel Mustard": [100, 200, 300]}
+donor_history[u"Mrs. White"] = [50, 10.95]
+donor_history[u"Professor Plum"] = [300, 300]
+donor_history["Mr. Green"] = [1000, 2000]
+donor_history["Miss Scarlet"] = [1000]
+def donorssort((i, donations)):
+    return sum(donations)
 
-option_indexes = [1, 2, 3]
-options = [u"Send a Thank You", u"Create a Report", u"Exit"]
+options = {1: u"Send a Thank You", 2: u"Create a Report", 3: u"Exit"}
+def optionsort((i, str)):
+    return i
 
 def safe_input(prompt_string):
     """Returns user input, None if ^C or ^D"""
@@ -20,10 +23,10 @@ def safe_input(prompt_string):
 def initial_prompt():
     """Return an int representing the user's selection among the options"""
     print u"Please select from the following options: "
-    i = 0
-    for index in option_indexes:
-        print u"%i) %s" % (index, options[i])
-        i = i + 1
+    option_list = options.items()
+    option_list.sort(key=optionsort)
+    for index, desc in option_list:
+        print u"%i) %s" % (index, desc)
     while True:
         try:
             selection = int(safe_input(u"Enter the number of your selection: "))
@@ -31,7 +34,7 @@ def initial_prompt():
         except (ValueError, TypeError):
             print u"I didn't understand that, please enter a number"
 
-    if selection not in option_indexes:
+    if selection not in options:
         print u"Invalid selection\n\a"
         selection = initial_prompt()
     return selection
@@ -39,8 +42,8 @@ def initial_prompt():
 def list_donors():
     """List full names of donors"""
     print u"We have records for the following donors"
-    for record in donor_history:
-        print record[0]
+    for key in donor_history:
+        print key
 
 def send_thankyou():
     """Generate a thank you note for an individual donor"""
@@ -52,13 +55,8 @@ def send_thankyou():
     if donor == u"quit":
         return
 
-    for record in donor_history:
-        if record[0] == donor:
-            selected_record = record
-            break
-    else:
-        selected_record = (donor, [])
-        donor_history.append(selected_record)
+    if not donor_history.has_key(donor):
+        donor_history[donor] = []
 
     donation = safe_input(u"Enter the donation amount: ")
 
@@ -75,11 +73,11 @@ def send_thankyou():
             print u"I didn't understand that, please enter a number."
             donation = safe_input(u"Enter the donation amount: ")
 
-    selected_record[1].append(donation)
+    donor_history[donor].append(donation)
 
     print u"\n\n\n"
-    print u"Dear %s:" % selected_record[0]
-    print u"Thank you so much for your recent donation of $%.2f. This means so" % selected_record[1][-1]
+    print u"Dear %s:" % donor
+    print u"Thank you so much for your recent donation of $%.2f. This means so" % donor_history[donor][-1]
     print u"much to the little orphaned children of Mr. Boddy, who recently perished"
     print u"in his own mansion under mysterious circumstances."
     print u"Sincerely,"
@@ -88,9 +86,10 @@ def send_thankyou():
 
 def create_report():
     print u"\n\n\n"
-    donor_history.sort(key=lambda record: sum(record[1]))
+    donor_history_list = donor_history.items()
+    donor_history_list.sort(key=donorssort)
     print u"%-20s%12s%8s%12s" % (u"Name", u"Total", u"No.", u"Avg.")
-    for record in donor_history:
+    for record in donor_history_list: #Could have this use the dict, but I don't see why
         total = sum(record[1])
         no = len(record[1])
         avg = float(total) / float(no)
@@ -105,7 +104,4 @@ if __name__ == '__main__':
         if user_selection == 2:
             create_report()
         user_selection = initial_prompt()
-
-
-
 
