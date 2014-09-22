ed685ad76160882ea8e3d1c708701332154c22e1
jbbrokaw <jbrokaw@gmail.com>
2014-09-21 17:49:34 -0700

    Creates executable script that generates some Sherlock Holmes

--- Students/JasonBrokaw/session04/trigrams.py
+++ Students/JasonBrokaw/session04/trigrams.py
@@ -1,4 +1,8 @@
-import urllib #, string
+#!/usr/local/bin/python
+
+import urllib#, string
+import random, textwrap
+
 
 base_text_file = urllib.urlopen(u"http://codefellows.github.io/sea-c15-python/_downloads/sherlock.txt")
 
@@ -14,14 +18,25 @@ base_text_file.close()
 
 base_list = base_text.split()
 
-trigrams = {} #Each key will be a pair of words, and the value will be a set (no repeats) of words that follow that pair
+trigrams = {} #Each key will be a pair of words, and the value will be a list of words that follow that pair
 
 for i in range(len(base_list) - 2):
     key = " ".join(base_list[i:i+2])
     if trigrams.has_key(key):
-        trigrams[key].add(base_list[i+2])
+        trigrams[key].append(base_list[i+2])
     else:
-        trigrams[key] = set(base_list[i+2])
+        trigrams[key] = [base_list[i+2]]
+
+def generate_text(length=500, starting_key="Sherlock Holmes"):
+    if not trigrams.has_key(starting_key):
+        starting_key = "Sherlock Holmes"
+        print "That is not a valid key, using 'Sherlock Holmes'"
+    output = starting_key.split() #build this up as a word list
+    key = starting_key
+    for i in range(length):
+        output.append(random.sample(trigrams[key],1)[0])
+        key = " ".join(output[-2:])
+    print textwrap.fill(" ".join(output))
 
 # print trigrams
 
@@ -29,4 +44,5 @@ for i in range(len(base_list) - 2):
 #     if len(trigrams[key]) > 10:
 #         print key, trigrams[key]
 
-
+if __name__ == '__main__':
+    generate_text()
