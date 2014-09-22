aa0dc0730140eee90426172e05caca4db9f214e6
jbbrokaw <jbrokaw@gmail.com>
2014-09-21 18:02:02 -0700

    Adds variable key_length to play around with

--- Students/JasonBrokaw/session04/trigrams.py
+++ Students/JasonBrokaw/session04/trigrams.py
@@ -15,22 +15,27 @@ base_list = base_text.split()
 
 trigrams = {} #Each key will be a pair of words, and the value will be a list of words that follow that pair
 
-for i in range(len(base_list) - 2):
-    key = " ".join(base_list[i:i+2])
-    if trigrams.has_key(key):
-        trigrams[key].append(base_list[i+2])
-    else:
-        trigrams[key] = [base_list[i+2]]
+key_length = 1
 
-def generate_text(length=500, starting_key="Sherlock Holmes"):
+for i in range(len(base_list) - key_length):
+    key = " ".join(base_list[i:i+key_length])
+    if trigrams.has_key(key):
+        trigrams[key].append(base_list[i+key_length])
+    else:
+        trigrams[key] = [base_list[i+key_length]]
+
+
+default_key = " ".join(base_list[267:267+key_length]) #This starts at "world has seen but as a lover ..."
+
+def generate_text(length=500, starting_key=default_key):
     if not trigrams.has_key(starting_key):
-        starting_key = "Sherlock Holmes"
-        print "That is not a valid key, using 'Sherlock Holmes'"
+        starting_key = default_key
+        print "That is not a valid key, using '" + starting_key + "'"
     output = starting_key.split() #build this up as a word list
     key = starting_key
     for i in range(length):
         output.append(random.sample(trigrams[key],1)[0])
-        key = " ".join(output[-2:])
+        key = " ".join(output[-key_length:])
     print textwrap.fill(" ".join(output))
 
 if __name__ == '__main__':
