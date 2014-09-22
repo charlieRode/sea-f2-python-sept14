cc8688f49122674271818bcac73415408c4fc2b6
James Warren <jwarren116@gmail.com>
2014-09-21 21:39:39 -0700

    Build framework for report module of mailroom.py

--- Students/JamesWarren/session03/mailroom.py
+++ Students/JamesWarren/session03/mailroom.py
@@ -1,13 +1,12 @@
 #!/usr/env/python
 
-donor_list = [
-    ("John Doner", [1500]),
-    ("Mary Doner", [900, 1800, 2000]),
-    ("Joe Smith", [100, 150, 175]),
-    ("Bob Andrews", [250, 250]),
-    ("Cindy Brown", [800])
-]
-
+donor_list = {
+    "John Doner": [1500],
+    "Mary Doner": [900, 1800, 2000],
+    "Joe Smith": [100, 150, 175],
+    "Bob Andrews": [250, 250],
+    "Cindy Brown": [800]
+}
 
 
 def main_menu():
@@ -16,26 +15,27 @@ def main_menu():
     #SEND THANK YOU
     if menu_select == "1":
         
-        user_inp = raw_input("Please enter donor's full name. > ")
+        print "To return to the main menu at any point, type 'HOME'."
+        user_inp = raw_input("Please enter donor's full name, or enter 'list' to see complete list of donors. > ")
+        while user_inp == "HOME":
+            main_menu()
         while user_inp == "list":
             print donor_list
             user_inp = raw_input("Please enter donor's full name. > ")
         if user_inp not in donor_list:
             donation = int(raw_input("Donation amount? > "))
-            #while not donation.isdigit():
-                #donation = raw_input("Donation amount? > ")
-            donor_list.append((user_inp,[donation]))
+            while not type(donation) == int:
+                donation = raw_input("Donation amount? > ")
+            donor_list.setdefault(user_inp,[]).append[donation]
 
-        if user_inp == "HOME":
-            main_menu()
+        print thank_you(user_inp, donation)
+
 
     #CREATE REPORT
     elif menu_select == "2":
-        pass
+        print report()
 
-
-
-def thank_you(name):
+def thank_you(name,amount):
     return """Dear %s,
 
     Thank you so much for your generous donation of $%i! Every dollar
@@ -43,13 +43,23 @@ def thank_you(name):
     the generosity of loyal donors like you.
 
     Sincerely,
-    Bob Smith""" % (donor, amount)
-
-
+    Bob Smith""" % (name, amount)
 
 def report():
-    pass
+    return """
+    Name . . . Total Donated . . . No. Donations . . . Avg. Donation
+    %s         %i                  %i                  %i
+    %s         %i                  %i                  %i
+    %s         %i                  %i                  %i
+    %s         %i                  %i                  %i
+    %s         %i                  %i                  %i
+    """ % (donor_list[], tot, len(), (tot/len(donor_list.values())), \
+        donor_list[], tot, len(), (tot/len()), \
+        donor_list[], tot, len(), (tot/len()), \
+        donor_list[], tot, len(), (tot/len()), \
+        donor_list[], tot, len(), (tot/len()))
 
+    main_menu()
 
 
 if __name__ == '__main__':
