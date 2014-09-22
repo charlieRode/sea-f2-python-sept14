94ac152c5af8af5ba1e1231aaf5fd7e98a6a0440
georgen <george.nuesca@gmail.com>
2014-09-22 08:47:24 -0700

    Initial

---
+++ Students/GeorgeNuesca/session04/dict_sets.py
@@ -0,0 +1,26 @@
+
+
+
+dessert = {'name' : 'Chris', 'city' : 'Seattle', 'Cake' : 'chocolate'}
+print dessert
+
+dessert.pop('Cake', None)
+print dessert
+
+# del dessert['Cake']
+# print dessert
+
+dessert['fruit'] = 'Mango', 'cake'
+print dessert
+
+print dessert
+print dessert.keys()
+print dessert.values()
+
+print 'cake' in dessert.keys()
+print 'cake' in dessert.values()
+print 'cake' in dessert
+print 'Mango' in dessert.values()
+
+#automate the creation of a list of numbers
+#automate the creation of corresponding hex numbers referencing the other list
