a3331a1e801da39369bdd4e0918f08e6405bac0c
James Warren <jwarren116@gmail.com>
2014-09-23 16:18:53 -0700

    Add safe_input function to mailroom.py

--- Students/JamesWarren/session03/mailroom.py
+++ Students/JamesWarren/session03/mailroom.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import io
 
 donor_list = {
     "John Doner": [1500],
@@ -8,26 +9,33 @@ donor_list = {
     "Cindy Brown": [800]
 }
 
+def safe_input(question):
+    try:
+        return raw_input(question)
+    except EOFError:
+        return None
+    except KeyboardInterrupt:
+        return None
 
 def main_menu():
     print "Type Exit to quit."
-    menu_select = raw_input("Please select: (1) Send a thank you note; \
+    menu_select = safe_input("Please select: (1) Send a thank you note; \
 (2) Create a report > ")
     
     #SEND THANK YOU
     if menu_select == "1":
-        print "To return to the main menu at any point, type 'HOME'."
-        user_inp = raw_input("Please enter donor's full name, or type \
+        print "To return to the main menu, type 'HOME'."
+        user_inp = safe_input("Please enter donor's full name, or type \
 'list' to see complete list of donors. > ")
         if user_inp == "HOME":
             main_menu()
         if user_inp == "list":
             for (key, value) in donor_list.items():
                 print (key, value)
-            user_inp = raw_input("Please enter donor's full name. > ")
+            user_inp = safe_input("Please enter donor's full name. > ")
         if user_inp not in donor_list:
             while True:
-                donation = raw_input("Donation amount? > ")
+                donation = safe_input("Donation amount? > ")
                 try:
                     donation = int(donation)
                     break
@@ -46,14 +54,18 @@ def main_menu():
         print report()
 
 def thank_you(name,amount):
-    return """Dear %s,
+    f = io.open('thank_you.txt', mode='w')
+    f.write(u"""Dear %s, \
+ \
+Thank you so much for your generous donation of $%i! Every dollar \
+helps local families in need, and none of it would be possible without \
+the generosity of loyal donors like you. \
+\
+Sincerely, \
+Bob Smith""" % (name, amount))
+    f.close()
+    print "Your thank you note has been saved to thank_you.txt"
 
-Thank you so much for your generous donation of $%i! Every dollar
-helps local families in need, and none of it would be possible without
-the generosity of loyal donors like you.
-
-Sincerely,
-Bob Smith""" % (name, amount)
 
 def report():
 #    return """
@@ -72,6 +84,5 @@ def report():
     main_menu()
 
 
-if __name__ == '__main__':
-    
+if __name__ == '__main__':  
     main_menu()
