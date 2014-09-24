d44b28a0bda9b3ee29178c0f27c676d87ede91fb
James Warren <jwarren116@gmail.com>
2014-09-24 13:45:28 -0700

    Finish Report module

--- Students/JamesWarren/session03/mailroom.py
+++ Students/JamesWarren/session03/mailroom.py
@@ -10,79 +10,83 @@ donor_list = {
 }
 
 def safe_input(question):
-    try:
-        return raw_input(question)
-    except EOFError:
-        return None
-    except KeyboardInterrupt:
-        return None
+    while True:
+        try:
+            return raw_input(question)
+            break
+        except EOFError:
+            return None
+        except KeyboardInterrupt:
+            return None
 
 def main_menu():
-    print "Type Exit to quit."
+    print "---------- HOME ----------"
+    print
+    print "Type EXIT to quit."
     menu_select = safe_input("Please select: (1) Send a thank you note; \
 (2) Create a report > ")
     
     #SEND THANK YOU
     if menu_select == "1":
+        print
+        print "---------- THANK YOU ----------"
+        print
         print "To return to the main menu, type 'HOME'."
         user_inp = safe_input("Please enter donor's full name, or type \
 'list' to see complete list of donors. > ")
         if user_inp == "HOME":
             main_menu()
         if user_inp == "list":
+            print
             for (key, value) in donor_list.items():
                 print (key, value)
             user_inp = safe_input("Please enter donor's full name. > ")
-        if user_inp not in donor_list:
-            while True:
-                donation = safe_input("Donation amount? > ")
-                try:
-                    donation = int(donation)
-                    break
-                except ValueError:
-                    print "Please enter a valid number."
-                    continue
-
-            donor_list.setdefault(user_inp, []).append(donation)
-
+        while True:
+            donation = safe_input("Donation amount? > ")
+            try:
+                donation = int(donation)
+                break
+            except ValueError:
+                print "Please enter a valid number."
+                continue
+        donor_list.setdefault(user_inp, []).append(donation)
+        print
         print thank_you(user_inp, donation)
         main_menu()
 
 
     #CREATE REPORT
     elif menu_select == "2":
-        print report()
+        report()
 
 def thank_you(name,amount):
-    f = io.open('thank_you.txt', mode='w')
-    f.write(u"""Dear %s, \
- \
-Thank you so much for your generous donation of $%i! Every dollar \
-helps local families in need, and none of it would be possible without \
-the generosity of loyal donors like you. \
-\
-Sincerely, \
+    f = io.open('thank_you_%s.txt' % name, mode='w')
+    f.write(u"""Dear %s,
+
+Thank you so much for your generous donation of $%i! Every dollar 
+helps local families in need, and none of it would be possible without 
+the generosity of loyal donors like you. 
+
+Sincerely, 
 Bob Smith""" % (name, amount))
     f.close()
-    print "Your thank you note has been saved to thank_you.txt"
-
+    print "Your thank you note has been saved to thank_you_%s.txt" % name
 
 def report():
-#    return """
-#    Name . . . Total Donated . . . No. Donations . . . Avg. Donation
-#    %s         %i                  %i                  %i
-#    %s         %i                  %i                  %i
-#    %s         %i                  %i                  %i
-#    %s         %i                  %i                  %i
-#    %s         %i                  %i                  %i
-#    """ % (donor_list[], tot, len(), (tot/len(donor_list.values())), \
-#        donor_list[], tot, len(), (tot/len()), \
-#        donor_list[], tot, len(), (tot/len()), \
-#        donor_list[], tot, len(), (tot/len()), \
-#        donor_list[], tot, len(), (tot/len()))
-    print "Report here"
+    print
+    print "---------- REPORT ----------"
+    print
+    print "Name".ljust(24) + "Total Donated".ljust(16) + \
+    "No. Donations".ljust(16) + "Avg. Donation".ljust(16)
+    for key, value in donor_list.iteritems():
+        tot_donations = sum(value)
+        num_donations = len(value)
+        avg_donations = tot_donations / num_donations
+        print key.ljust(24) + "$"+str(tot_donations).ljust(15) + \
+        str(num_donations).ljust(16) + "$"+str(avg_donations).ljust(15)
+    print
+    print "End of report"
     main_menu()
-
 
 if __name__ == '__main__':  
     main_menu()
