5d8baf9c162f1c5c4665201e37235c8df6234a08
jbbrokaw <jbrokaw@gmail.com>
2014-09-27 17:59:21 -0700

    Adds clean_file() without list comprehension

---
+++ Students/JasonBrokaw/session05/clean-file.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+import sys, io
+
+def clean_file(*args):
+    """Remove all leading and trailing whitespace from each line of a file"""
+    if not args:
+        print "usage: ./clean-file.py input-filename [output-filename]"
+        print "or clean_file(input-filename[, output-filename])"
+        print "If output-filename is not specified, the input file will be overwritten"
+        return
+
+    infile = io.open(args[0], 'r')
+
+    lines = infile.readlines()
+
+    infile.close()
+
+    output_filename = args[1] if len(args) > 1 else args[0]
+    outfile = io.open(output_filename, 'w', encoding='utf-8')
+
+    for line in lines:
+        outfile.write(line.strip() + "\n")
+    outfile.close()
+
+if __name__ == '__main__':
+    clean_file(*sys.argv[1:])
