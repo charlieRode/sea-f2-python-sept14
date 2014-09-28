fac7f0307514ab1d0a441383080e7f7a7226bbc8
James Warren <jwarren116@gmail.com>
2014-09-27 22:06:36 -0700

    Part 3, html_render.py

--- Students/JamesWarren/session06/html_render.py
+++ Students/JamesWarren/session06/html_render.py
@@ -2,7 +2,7 @@
 
 class Element(object):
     tag = "html"
-    indent = ""
+    ind = ""
     
     def __init__(self, content=None):
         if content is None:
@@ -16,14 +16,15 @@ class Element(object):
 
     def render(self, file_out, ind=""):
         """ render the content to the given file """
+        cind = ind
         ind += "    "
-        file_out.write("<"+self.tag+">\n"+ind)
+        file_out.write("<%s>\n%s" % (self.tag, ind))
         for item in self.content:
             try:
                 item.render(file_out, ind)
             except AttributeError:
                 file_out.write(("\n"+ind).join(self.content))
-        file_out.write(ind+"\n</" + self.tag + ">")
+        file_out.write("\n%s</%s>\n%s" % (cind, self.tag, cind))
 
 class Html(Element):
     tag = "html"
@@ -33,3 +34,14 @@ class Body(Element):
 
 class P(Element):
     tag = "p"
+
+class Head(Element):
+    tag = "head"
+
+class OneLineTag(Element):
+    def render(self, file_out, ind=""):
+        ind += "    "
+        file_out.write("<%s> %s </%s>%s" % (self.tag, self.content, self.tag, ind))
+
+class Title(OneLineTag):
+    tag = "title"
