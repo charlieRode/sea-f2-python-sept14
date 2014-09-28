d890ca035b41a4823117cc50cd421fef2a18b2d0
jbbrokaw <jbrokaw@gmail.com>
2014-09-27 18:14:48 -0700

    Changes approach to handling single argument, adds warning

--- Students/JasonBrokaw/session05/clean-file.py
+++ Students/JasonBrokaw/session05/clean-file.py
@@ -3,43 +3,49 @@
 from __future__ import unicode_literals
 import sys, io
 
-def clean_file(*args):
-    """Remove all leading and trailing whitespace from each line of a file"""
-    if not args:
-        print "usage: ./clean-file.py input-filename [output-filename]"
-        print "or clean_file(input-filename[, output-filename])"
-        print "If output-filename is not specified, the input file will be overwritten"
-        return
+# def clean_file(*args):
+#     """Remove all leading and trailing whitespace from each line of a file"""
+#     if not args:
+#         print "usage: ./clean-file.py input-filename [output-filename]"
+#         print "or clean_file(input-filename[, output-filename])"
+#         print "If output-filename is not specified, the input file will be overwritten"
+#         return
 
-    infile = io.open(args[0], 'r')
+#     infile = io.open(args[0], 'r')
 
-    lines = infile.readlines()
+#     lines = infile.readlines()
 
-    infile.close()
+#     infile.close()
 
-    output_filename = args[1] if len(args) > 1 else args[0]
-    outfile = io.open(output_filename, 'w', encoding='utf-8')
+#     output_filename = args[1] if len(args) > 1 else args[0]
+#     outfile = io.open(output_filename, 'w', encoding='utf-8')
 
-    for line in lines:
-        outfile.write(line.strip() + "\n")
-    outfile.close()
+#     for line in lines:
+#         outfile.write(line.strip() + "\n")
+#     outfile.close()
 
 def clean_file_comprehension(*args):
     """Remove all leading and trailing whitespace from each line of a file"""
     if not args:
         print "usage: ./clean-file.py input-filename [output-filename]"
-        print "or clean_file(input-filename[, output-filename])"
+        print "or clean_file_comprehension(input-filename[, output-filename])"
         print "If output-filename is not specified, the input file will be overwritten"
         return
-
-    infile = io.open(args[0], 'r')
+    if len(args) > 1:
+        infile = io.open(args[0], 'r', encoding='utf-8')
+        outfile = io.open(args[1], 'w', encoding='utf-8')
+    else:
+        if raw_input("Overwriting file, are you sure (type 'yes'): ") != 'yes':
+            print "usage: ./clean-file.py input-filename [output-filename]"
+            print "or clean_file_comprehension(input-filename[, output-filename])"
+            print "If output-filename is not specified, the input file will be overwritten"
+            return
+        infile = io.open(args[0], 'r+', encoding='utf-8')
+        outfile = infile
 
     lines = infile.readlines()
 
-    infile.close()
-
-    output_filename = args[1] if len(args) > 1 else args[0]
-    outfile = io.open(output_filename, 'w', encoding='utf-8')
+    outfile.seek(0)
 
     outfile.writelines([line.strip() + "\n" for line in lines])
     outfile.close()
