d21992dd94e9d23ecfd16dd3497d6d0c8df65a59
jbbrokaw <jbrokaw@gmail.com>
2014-09-27 18:04:18 -0700

    Adds a version with a comprehension

--- Students/JasonBrokaw/session05/clean-file.py
+++ Students/JasonBrokaw/session05/clean-file.py
@@ -24,5 +24,25 @@ def clean_file(*args):
         outfile.write(line.strip() + "\n")
     outfile.close()
 
+def clean_file_comprehension(*args):
+    """Remove all leading and trailing whitespace from each line of a file"""
+    if not args:
+        print "usage: ./clean-file.py input-filename [output-filename]"
+        print "or clean_file(input-filename[, output-filename])"
+        print "If output-filename is not specified, the input file will be overwritten"
+        return
+
+    infile = io.open(args[0], 'r')
+
+    lines = infile.readlines()
+
+    infile.close()
+
+    output_filename = args[1] if len(args) > 1 else args[0]
+    outfile = io.open(output_filename, 'w', encoding='utf-8')
+
+    outfile.writelines([line.strip() + "\n" for line in lines])
+    outfile.close()
+
 if __name__ == '__main__':
-    clean_file(*sys.argv[1:])
+    clean_file_comprehension(*sys.argv[1:])
